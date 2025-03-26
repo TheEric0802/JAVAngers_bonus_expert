83dad99ebc695341c09f7607194909af6229e075
Eric Schmidt <eric0802@web.de>
2025-03-26 15:11:18 +0100

    sort an int array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
         System.out.println("Type in a String to be reversed: ");
         String inputString = input.nextLine();
+
         String reversed = new StringBuilder(inputString).reverse().toString();
         System.out.println(reversed);
+
+        int[] numbers = {6, 2, 8, 3, 9, 6};
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
