ca6c833c8755d41f0d3e6435447e890378d35adb
Eric Schmidt <eric0802@web.de>
2025-03-26 15:05:59 +0100

    put in a string and put out the reversed String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type in a String to be reversed: ");
+        String inputString = input.nextLine();
+        String reversed = new StringBuilder(inputString).reverse().toString();
+        System.out.println(reversed);
     }
 }
